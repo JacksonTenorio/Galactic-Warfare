1a885da5801510186acd01f4fd54394abe19555f
Jackson Tenório <kleber@gmail.com>
2023-05-09 22:31:45 -0300

    Upload 09/05/23

--- Galactic Warfare/Assets/Scripts/Cam.cs
+++ Galactic Warfare/Assets/Scripts/Cam.cs
@@ -1,26 +1,21 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TiroRapido : MonoBehaviour
+public class Cam : MonoBehaviour
 {
     private Rigidbody2D rig;
-
     public float velocidade;
     
+    // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        Destroy(gameObject, 1.4f);
     }
+
+    // Update is called once per frame
     void FixedUpdate()
     {
         rig.velocity = Vector2.right * velocidade;
-    }
-
-    private void OnTriggerEnter(Collider col)
-    {
-        
     }
 }
