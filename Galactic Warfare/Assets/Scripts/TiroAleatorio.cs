854907ca95d4ec99425084dd7c0a122f263d2d76
Jackson Tenório <kleber@gmail.com>
2023-05-15 11:43:54 -0300

    Upload Galactic Warfare 15/05/23

--- Galactic Warfare/Assets/Scripts/TiroAleatorio.cs
+++ Galactic Warfare/Assets/Scripts/TiroAleatorio.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class TiroAleatorio : MonoBehaviour
 {
     public Transform _FirePoint;
+    public Transform _FirePoint2;
     public GameObject _BulletPrefab;
+    
     public float _FireRate = 2f;
     public float _Speed = 10f;
-
+    public bool _Enemy2 = false;
     private float _FireTimer = 0f;
+
+    public float _Range;
+    public float _Range2;
 
     // Update is called once per frame
     void Update()
@@ -24,12 +29,18 @@ public class TiroAleatorio : MonoBehaviour
             _FireTimer = 0f;
 
             // Calcula a direção de bala
-            float angle = Random.Range(60f, -61f);
+            float angle = Random.Range(_Range, _Range2);
             Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
             Vector2 direction = rotation * Vector2.left;
 
             // Cria a bala e adicona a velocidade
-            GameObject bullet = Instantiate(_BulletPrefab, _FirePoint.position, rotation/*Quaternion.identity*/);
+            GameObject bullet = Instantiate(_BulletPrefab, _FirePoint.position, rotation);
+            if (_Enemy2 == true)
+            {
+                GameObject bullet2 = Instantiate(_BulletPrefab, _FirePoint2.position, rotation);
+                Rigidbody2D bulletRb2 = bullet2.GetComponent<Rigidbody2D>();
+                bulletRb2.velocity = direction * _Speed;
+            }
             Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
             bulletRb.velocity = direction * _Speed;
             Invoke("Destruction", 2f);
