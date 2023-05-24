f13f5d8a950d98aad009d1dc4301aa9e351a3e5f
Jackson Tenório <kleber@gmail.com>
2023-05-24 08:55:51 -0300

    bora

--- Galactic Warfare/Assets/Scripts/EnemyHP.cs
+++ Galactic Warfare/Assets/Scripts/EnemyHP.cs
@@ -5,14 +5,92 @@ using UnityEngine;
 
 public class EnemyHP : MonoBehaviour
 {
-    public static GameObject _escudo;
+    private ScoreManager scoreManager;
+    
+    public bool _Enemy1;
+    public bool _Enemy2;
+    public bool _Enemy3;
+
+    public GameObject _escudo;
 
     public int _MaxHP;
     public static int _HPAtual;
-    public static int _escudoHP = 3;
-
+    
+    public int _escudoHPMax = 3;
+    public int _escudoHPAtual;
+    private bool _escudoAtivado;
+    
     private void Start()
     {
-        _escudo = GameObject.Find("Escudo");
+        scoreManager = GameObject.Find("GameManager").GetComponent<ScoreManager>();
+        _HPAtual = _MaxHP;
+        _escudoHPAtual = _escudoHPMax;
+        _escudoAtivado = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (_Enemy1 == true && col.gameObject.tag == "Bala")
+        {
+            _HPAtual -= 1;
+            Destroy(col.gameObject);
+
+            if (_HPAtual <= 0)
+            {
+                Destroy(gameObject);
+                scoreManager.IncreaseScore();
+            }
+        }
+        if (_Enemy2 == true && col.gameObject.tag == "Bala")
+        {
+            _escudo.SetActive(true);
+            _escudoAtivado = true;
+            
+            _escudoHPAtual -= 1;
+            Destroy(col.gameObject);
+
+            if (_escudoHPAtual <= 0)
+            {
+                _escudo.SetActive(false);
+                _escudoAtivado = false;
+            }
+        }
+        if (_Enemy2 == true && _escudoAtivado == false && col.gameObject.tag == "Bala")
+        {
+            _HPAtual -= 1;
+            Destroy(col.gameObject);
+
+            if (_HPAtual <= 0)
+            {
+                Destroy(gameObject);
+                scoreManager.IncreaseScore();
+            }
+        }
+        
+        if (_Enemy3 == true && col.gameObject.tag == "Bala")
+        {
+            _escudo.SetActive(true);
+            _escudoAtivado = true;
+            
+            _escudoHPAtual -= 1;
+            Destroy(col.gameObject);
+
+            if (_escudoHPAtual <= 0)
+            {
+                _escudo.SetActive(false);
+                _escudoAtivado = false;
+            }
+        }
+        if (_Enemy3 == true && _escudoAtivado == false && col.gameObject.tag == "Bala")
+        {
+            _HPAtual -= 1;
+            Destroy(col.gameObject);
+
+            if (_HPAtual <= 0)
+            {
+                Destroy(gameObject);
+                scoreManager.IncreaseScore();
+            }
+        }
     }
 }
--- Galactic Warfare/Assets/Scripts/TiroRapido.cs
+++ Galactic Warfare/Assets/Scripts/TiroRapido.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class TiroRapido : MonoBehaviour
 {
-    private ScoreManager scoreManager;
     private Rigidbody2D rig;
 
     public float velocidade;
@@ -13,45 +12,10 @@ public class TiroRapido : MonoBehaviour
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
-        scoreManager = GameObject.Find("GameManager").GetComponent<ScoreManager>();
     }
     void FixedUpdate()
     {
         rig.velocity = Vector2.right * velocidade;
         Destroy(gameObject, 1.5f);
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Enemy"))
-        {
-            Destroy(gameObject);
-            if (EnemyHP._escudoHP > 0)
-            {
-                EnemyHP._escudoHP -= 1;
-                Destroy(gameObject);
-            }
-            else
-            {
-                Destroy(EnemyHP._escudo);
-                Destroy(gameObject);
-            }
-
-            if (EnemyHP._escudo == null)
-            {
-                Destroy(gameObject);
-                if (EnemyHP._HPAtual >0)
-                {
-                    Destroy(gameObject);
-                    EnemyHP._HPAtual -= 1;
-                }
-                else
-                {
-                    Destroy(gameObject);
-                    Destroy(collision.gameObject);
-                    scoreManager.IncreaseScore();
-                }
-            }
-        }
     }
 }
