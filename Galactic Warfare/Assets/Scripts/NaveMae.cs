b92079272666a8577b7434358c29a663308a2567
Jackson <antoniojacksontenorio@gmail.com>
2023-08-27 12:20:36 -0300

    Upload Nave Mãe 1.0

--- Galactic Warfare/Assets/Scripts/NaveMae.cs
+++ Galactic Warfare/Assets/Scripts/NaveMae.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -12,10 +13,15 @@ public class NaveMae : MonoBehaviour
     
     public GameObject _Laser;
     public GameObject _Missel;
+    public Vector2 _MisselPosition;
     public GameObject _LaserQueSegue;
+    private GameObject _Player;
+    private Vector3 _PlayerPosition;
+    private Vector2 vec2;
 
-    private float _SpeedFire;
-    private float _Timer;
+    public float _SpeedFire;
+    private float _Timer1;
+    private float _Timer2;
 
     private bool Nvl1;
     private bool Nvl2;
@@ -24,19 +30,19 @@ public class NaveMae : MonoBehaviour
     // ===== NaveMãe =====
     private Rigidbody2D _Rig;
 
-    public int _Speed;
-    
+    public float _Speed;
+
     // ===== Escudo =====
     public GameObject _Escudo;
 
-    public int _EscudoVidaMax;
-    private int _EscudoVidaMin;
+    public float _EscudoVidaMax;
+    private float _EscudoVidaMin;
     
     // ===== Vida =====
     public Slider _BarraDeVida;
 
-    public int _VidaMax;
-    private int _VidaMin;
+    public float _VidaMax;
+    private float _VidaAtual;
     
 
     // Start is called before the first frame update
@@ -47,31 +53,129 @@ public class NaveMae : MonoBehaviour
         Nvl1 = true;
         Nvl2 = false;
         Nvl3 = false;
+        
+        _Timer1 = 2;
+        _Timer2 = 2;
+
+        _VidaAtual = _VidaMax;
+        VidaUpdate();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        _Player = GameObject.FindGameObjectWithTag("Player");
+        _PlayerPosition = _Player.transform.position;
+
+        _MisselPosition = _Missel.transform.position;
+        
         Move();
+
+        if (Nvl1)
+        {
+            Laser();
+        }
+        if (Nvl2)
+        {
+            Missel();
+        }
+        if (Nvl3)
+        {
+            _LaserQueSegue.SetActive(true);
+            LaserQueSegue();
+        }
+        if (!Nvl3)
+        {
+            _LaserQueSegue.SetActive(false);
+        }
+        
     }
 
-    void Move()
+    public void Move()
     {
         _Rig.velocity = Vector2.right * _Speed;
     }
 
-    void Laser()
+    public void Laser()
     {
-        
+        _Timer1 -= Time.deltaTime;
+
+        if (_Timer1 <= 0)
+        {
+            // Calcula a direção de bala
+            float angle = Random.Range(-90, 91);
+            Quaternion rotation = Quaternion.Euler(0f, 0f, angle);
+            Vector2 direction = rotation * Vector2.left;
+            
+            // Cria a bala e adicona a velocidade
+            GameObject bullet = Instantiate(_Laser, _FirePosition.position, rotation);
+            Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+            bulletRb.velocity = direction * _SpeedFire;
+
+            _Timer1 = 2;
+        }
     }
 
-    void Missel()
+    public void Missel()
     {
+        _Timer2 -= Time.deltaTime;
         
+        if (_Timer2 <= 0)
+        {
+            Instantiate(_Missel, _FirePosition2.position, _Missel.transform.rotation);
+            Instantiate(_Missel, _FirePosition3.position, _Missel.transform.rotation);
+            
+            if (_MisselPosition.x >= _PlayerPosition.x)
+            {
+                _MisselPosition = Vector2.MoveTowards(_MisselPosition, _PlayerPosition, _SpeedFire * Time.deltaTime);
+            }
+            else
+            {
+                _Missel.GetComponent<Rigidbody2D>().velocity = Vector2.left * _SpeedFire;
+            }
+
+            _Timer2 = 2;
+        }
     }
 
-    void LaserQueSegue()
+    public void LaserQueSegue()
     {
-        
+        vec2 = (_LaserQueSegue.transform.position - _PlayerPosition).normalized;
+        float angle = Mathf.Atan2(vec2.y, vec2.x) * Mathf.Rad2Deg;
+        _LaserQueSegue.transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    public void VidaUpdate()
+    {
+        _BarraDeVida.value = _VidaAtual;
+
+        if (_VidaAtual >= 50)
+        {
+            Nvl1 = true;
+            Nvl2 = false;
+            Nvl3 = false;
+        }
+        else if (_VidaAtual >= 25 && _VidaAtual < 50)
+        {
+            Nvl1 = false;
+            Nvl2 = true;
+            Nvl3 = false;
+        }
+        else if (_VidaAtual > 0 && _VidaAtual < 25)
+        {
+            Nvl1 = false;
+            Nvl2 = false;
+            Nvl3 = true;
+        }
+        if (_VidaAtual <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void DanoEnemy(float _Dano = -10)
+    {
+        _VidaAtual -= _Dano;
+        VidaUpdate();
     }
 }
